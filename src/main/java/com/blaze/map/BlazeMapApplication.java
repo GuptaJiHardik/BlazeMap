f205584b3b4cf6c711e3ec0d0510cb40da787ca8
Hardik <hardikjiio25@gmail.com>
2025-04-18 14:05:15 +0530

    Intital project setup

---
+++ src/main/java/com/blaze/map/BlazeMapApplication.java
@@ -0,0 +1,13 @@
+package com.blaze.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazeMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlazeMapApplication.class, args);
+	}
+
+}
