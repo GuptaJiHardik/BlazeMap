a9c2cd812ba11a45cf9adbbb9551eab2a692e6ee
Hardik <hardikjiio25@gmail.com>
2025-06-03 18:53:29 +0530

    creating .env

--- src/main/java/com/blaze/map/BlazeMapApplication.java
+++ src/main/java/com/blaze/map/BlazeMapApplication.java
@@ -1,5 +1,6 @@
 package com.blaze.map;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlazeMapApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("KAFKA_BOOTSTRAP", dotenv.get("KAFKA_BOOTSTRAP"));
+		System.setProperty("REDIS_HOST", dotenv.get("REDIS_HOST"));
+		System.setProperty("REDIS_PORT", dotenv.get("REDIS_PORT"));
+
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+		System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
 		SpringApplication.run(BlazeMapApplication.class, args);
+
 	}
 
 }
